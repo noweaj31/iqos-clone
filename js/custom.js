ee15551e599e40dbf31b17d8b1677a882280185a
noweaj31 <noweajeel31@gmail.com>
2023-06-10 22:10:21 +0900

    feat:iqos

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,28 @@
-$(document).ready(function(){
-  $('.slide-area').slick({
-  slidesToShow: 1,
-slidesToScroll: 1,
-autoplay: false,
-autoplaySpeed: 2000,
-arrows: true,
-prevArrow: $('#aro1_prev'),
-		nextArrow: $('#aro1_next'),
-})
-})
+$(document).ready(function () {
+  $(".slide-area").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: $("#aro1_prev"),
+    nextArrow: $("#aro1_next"),
+  });
+
+  $(".iqos-area-slick").slick({
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          speed: 300,
+          arrows: true,
+          prevArrow: $("#iaro1_prev"),
+          nextArrow: $("#iaro1_next"),
+        },
+      },
+    ],
+  });
+});
