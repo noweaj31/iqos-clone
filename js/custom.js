6aa32b6ae25393a61e522b49e979005b03478b5a
noweaj31 <noweajeel31@gmail.com>
2023-08-30 22:59:14 +0900

    feat:slick speed

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 800,
+    autoplaySpeed: 3000,
     arrows: true,
     prevArrow: $("#aro1_prev"),
     nextArrow: $("#aro1_next"),
