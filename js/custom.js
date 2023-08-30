73455d5e8240c70b9c4a5d73c513723bce7e639e
noweaj31 <noweajeel31@gmail.com>
2023-08-30 22:40:09 +0900

    feat:header 및 반응형 수정

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
   $(".slide-area").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 800,
     arrows: true,
     prevArrow: $("#aro1_prev"),
     nextArrow: $("#aro1_next"),
